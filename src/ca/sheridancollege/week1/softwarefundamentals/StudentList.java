f234c3f924f046d6d4eafbb4be9d1da066d48eb4
Faiyyad <patfaiyy@sheridancollege.ca>
2023-03-05 13:22:45 -0500

    Update java.java

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -15,14 +15,14 @@ public class StudentList {
     
     public static void main(String[] args)
     {
-       Student [] studentList = new Student[1];//change size before running
+       Student [] studentList = new Student[3];//change size before running
        Student s1 = new Student();
        s1.setName("Liz Dancy");
        studentList[0]=s1;
        //fill in 4 student names before running
        for(Student s: studentList)
        {
-           System.out.println(s.getName());
+           System.out.println("Enter Name : "+s.getName());
        }
         
     }
