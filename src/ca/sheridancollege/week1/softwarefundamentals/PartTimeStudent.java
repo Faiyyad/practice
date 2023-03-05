bf51e0785a3ffaf0ad8c2eeca9fbd97f0f32c61f
Faiyyad <patfaiyy@sheridancollege.ca>
2023-03-05 15:49:58 -0500

    Update java.java

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/PartTimeStudent.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author faiyy
+ */
+public class PartTimeStudent extends Student {
+    
+    private int numCourses;
+    
+}
